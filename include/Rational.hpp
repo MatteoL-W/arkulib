04ad77804da91eee0ae2e57d97c23ba2e97b61a1
MatteoL-petitordi <matteo.ftt@gmail.com>
2022-11-13 18:21:43 +0100

    Adding basic constructor

--- include/Rational.hpp
+++ include/Rational.hpp
@@ -7,19 +7,55 @@
  * @copyright WTFPL
  */
 
+#include <stdexcept>
+
 namespace Arkulib {
+    /**
+     * @brief An exception to handle divide by 0
+     */
+    class DivideByZeroException : public std::runtime_error {
+    public:
+        inline DivideByZeroException() : std::runtime_error("Denominator must not be null") {}
+    };
 
     /**
      * @brief This class can be used to express rationals
      * @tparam T
      */
     template<typename T = int>
-    class Ratio {
+    class Rational {
+
+    public:
+        /**
+         * @brief Default constructor : create a rational from a numerator and a denominator (set default at 1)
+         * @param numerator
+         * @param denominator
+         */
+        inline constexpr explicit Rational(const T numerator, const T denominator = 1)
+                : m_numerator(numerator), m_denominator(denominator) {
+            if (denominator == 0) {
+                throw DivideByZeroException();
+            }
+            // Faire pgcd
+        };
+
+        /**
+         * @defgroup constant_methods Methods which returns constants
+         * @brief Return a zero constant rational
+         * @return Rational<T>
+         */
+        inline constexpr Rational<T> Zero() { return Rational<T>(0, 1); };
+
+        /**
+         * @brief Return infinite constant rational
+         * @return Rational<T>
+         */
+        inline constexpr Rational<T> Infinite() { return Rational<T>(1, 0); };
 
     private:
-        T numerator; /*!< Rational's numerator */
+        T m_numerator; /*!< Rational's numerator */
 
-        T denominator; /*!< Rational's denominator */
+        T m_denominator; /*!< Rational's denominator */
 
     };
 
